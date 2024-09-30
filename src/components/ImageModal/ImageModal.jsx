7a399de6a5f26e001a8cb494e0cefff0b4cd47c3
RamiiaHolomoza <162986900+RamiiaHolomoza@users.noreply.github.com>
2024-10-01 00:46:27 +0300

    modal

--- src/components/ImageModal/ImageModal.jsx
+++ src/components/ImageModal/ImageModal.jsx
@@ -1,5 +1,5 @@
 import ReactModal from "react-modal";
-// import s from "./ImageModal.module.css";
+import s from "./ImageModal.module.css";
 
 const ImageModal = ({ modalImage, onClose }) => {
   if (!modalImage || !modalImage.regularUrl) {
@@ -7,9 +7,43 @@ const ImageModal = ({ modalImage, onClose }) => {
   }
 
   return (
-    <ReactModal isOpen={true} onRequestClose={onClose}>
-      <img src={modalImage.regularUrl} alt="Full-size image" />
-      <button onClick={onClose}>Close</button>
+    <ReactModal
+      isOpen={true}
+      onRequestClose={onClose}
+      shouldCloseOnOverlayClick={true}
+      shouldCloseOnEsc={true}
+      style={{
+        overlay: {
+          backgroundColor: "rgba(40, 40, 40, 0.8)",
+        },
+        content: {
+          position: "fixed",
+          top: "50%",
+          left: "50%",
+          transform: "translate(-50%, -50%)",
+          border: "1px solid white",
+          background: "transparent",
+          WebkitOverflowScrolling: "touch",
+          padding: "0",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          // width: "auto",
+          // height: "auto",
+          width: "1000px",
+          // max-width: "1000px",
+          height: "700px",
+        },
+      }}
+    >
+      <div className={s.imgWrap}>
+        <img
+          className={s.img}
+          src={modalImage.regularUrl}
+          alt="Full-size image"
+        />
+        {/* <button onClick={onClose}>Close</button> */}
+      </div>
     </ReactModal>
   );
 };
